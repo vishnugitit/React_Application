dfd265857e7bc53702b901cf733d2f6ffccda6e2
vishnugitit <vishnujangam1527@gmail.com>
2024-11-19 22:30:55 +0530

    deploy

---
+++ my-app/src/myapp.js
@@ -0,0 +1,16 @@
+import './myapp.css'
+function App(){
+return (
+    <div className="layout">
+        <h1 className='full'>I'm a Full Stack</h1>
+        <h1 className='web'>Web </h1>
+        <h1 className='developer'>Developer</h1>
+        <p className='quote1'>"Being a full-stack developer is not about knowing everything—it’s about knowing how to learn and adapt."
+        </p>
+        <p className='quote2'>"Every bug you fix takes you one step closer to mastering the stack."</p>
+        <p className='quote3'>"The best code is no code at all."</p>
+    </div>
+)
+}
+
+export default App
