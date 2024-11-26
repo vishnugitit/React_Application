766c997dbfcf434ea0d7e71bbf896a625a100841
vishnugitit <vishnujangam1527@gmail.com>
2024-11-26 17:00:00 +0530

    Commit

--- my-app/src/myapp.js
+++ my-app/src/myapp.js
@@ -1,16 +1,23 @@
-import './myapp.css'
-function App(){
-return (
+import "./myapp.css";
+function App() {
+  return (
     <div className="layout">
-        <h1 className='full'>I'm a Full Stack</h1>
-        <h1 className='web'>Web </h1>
-        <h1 className='developer'>Developer</h1>
-        <p className='quote1'>"Being a full-stack developer is not about knowing everything—it’s about knowing how to learn and adapt."
-        </p>
-        <p className='quote2'>"Every bug you fix takes you one step closer to mastering the stack."</p>
-        <p className='quote3'>"The best code is no code at all."</p>
+      <h1 className="full">I'm a Full Stack</h1>
+      <h1 className="web">Web </h1>
+      <h1 className="developer">Developer</h1>
+      <p className="quote1">
+        "Being a full-stack developer is not about knowing everything—it’s about
+        knowing how to learn and adapt."
+      </p>
+      <p className="quote2">
+        "Every bug you fix takes you one step closer to mastering the stack."
+      </p>
+      <p className="quote3">"The best code is no code at all."</p>
     </div>
-)
+  );
 }
 
-export default App
+export default App;
+
+//  cd my-app
+// npm start
